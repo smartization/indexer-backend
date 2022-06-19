3e9ab7ba82200995e91ca1c45070a7e389cb94bf
ptylczynski <piotr@ptl.cloud>
2022-06-20 00:11:37 +0200

    change: add mongo repositories to application

--- src/main/java/cloud/ptl/indexer/IndexerApplication.java
+++ src/main/java/cloud/ptl/indexer/IndexerApplication.java
@@ -2,8 +2,10 @@ package cloud.ptl.indexer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class IndexerApplication {
 
     public static void main(String[] args) {
