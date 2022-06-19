6e2f438a1885b50257f72dee97f92a7e9f4c84b2
ptylczynski <piotr@ptl.cloud>
2022-06-19 23:18:43 +0200

    init

---
+++ src/test/java/cloud/ptl/indexer/IndexerApplicationTests.java
@@ -0,0 +1,12 @@
+package cloud.ptl.indexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
