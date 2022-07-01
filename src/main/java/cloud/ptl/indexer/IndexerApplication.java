bd46d8d0c55ce9d840fe27d4a8b8d4499626775b
ptylczynski <piotr@ptl.cloud>
2022-07-02 01:31:47 +0200

    change: add default consumed media type

--- src/main/java/cloud/ptl/indexer/IndexerApplication.java
+++ src/main/java/cloud/ptl/indexer/IndexerApplication.java
@@ -1,14 +1,33 @@
 package cloud.ptl.indexer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@Slf4j
 public class IndexerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IndexerApplication.class, args);
+    }
+
+    @Configuration
+    public static class WebConfig implements WebMvcConfigurer {
+        @Override
+        // by default accepting all basic content types for post and put send via Postman
+        public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+            WebMvcConfigurer.super.configureContentNegotiation(configurer);
+            configurer.defaultContentType(MediaType.MULTIPART_FORM_DATA, MediaType.APPLICATION_FORM_URLENCODED,
+                            MediaType.APPLICATION_JSON)
+                    .parameterName("mediaType")
+                    .ignoreAcceptHeader(true);
+        }
     }
 }
