a40152047293756fe801a79493dae4222f79ad0f
ptylczynski <piotr@ptl.cloud>
2022-07-10 14:14:38 +0200

    change: move from mongo to postgres

--- src/main/java/cloud/ptl/indexer/IndexerApplication.java
+++ src/main/java/cloud/ptl/indexer/IndexerApplication.java
@@ -4,13 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableMongoRepositories
+@EnableJpaRepositories
 @Slf4j
 public class IndexerApplication {
 
