b1e5201100a7e34dfa06fefa26e3f815c24d2b65
ptylczynski <piotr@ptl.cloud>
2022-07-10 14:16:01 +0200

    change: type of id for ItemRepository

--- src/main/java/cloud/ptl/indexer/repositories/ItemRepository.java
+++ src/main/java/cloud/ptl/indexer/repositories/ItemRepository.java
@@ -3,5 +3,5 @@ package cloud.ptl.indexer.repositories;
 import cloud.ptl.indexer.model.ItemEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ItemRepository extends CrudRepository<ItemEntity, String> {
+public interface ItemRepository extends CrudRepository<ItemEntity, Long> {
 }
