467fad6014d4939e709ae24a1df3c40f686797d8
ptylczynski <piotr@ptl.cloud>
2022-08-24 16:00:30 +0200

    change: add no args and all args constructors to PlaceDTO

--- src/main/java/cloud/ptl/indexer/api/place/PlaceDTO.java
+++ src/main/java/cloud/ptl/indexer/api/place/PlaceDTO.java
@@ -2,14 +2,18 @@ package cloud.ptl.indexer.api.place;
 
 import cloud.ptl.indexer.model.PlaceEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlaceDTO {
     @Schema(
             description = "Internal place identifier",
