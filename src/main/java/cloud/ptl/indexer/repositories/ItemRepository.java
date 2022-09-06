abc85aa1bc3e65b95f0099d83b4033d2852eace3
ptylczynski <piotr@ptl.cloud>
2022-09-06 16:16:03 +0200

    change: add query to count items in given place

--- src/main/java/cloud/ptl/indexer/repositories/ItemRepository.java
+++ src/main/java/cloud/ptl/indexer/repositories/ItemRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface ItemRepository extends CrudRepository<ItemEntity, Long> {
     List<ItemEntity> findAllByStoragePlace_Id(Long placeId);
+    Long countByStoragePlace_Id(Long placeId);
 }
