462cc67df332bf7d028c91c4bb5800e63d0002b0
ptylczynski <piotr@ptl.cloud>
2022-09-06 16:16:24 +0200

    change: add service method to check number of items in given place

--- src/main/java/cloud/ptl/indexer/api/item/ItemService.java
+++ src/main/java/cloud/ptl/indexer/api/item/ItemService.java
@@ -43,4 +43,22 @@ public class ItemService {
     public List<ItemEntity> getItemsOnPlace(Long placeId) {
         return itemRepository.findAllByStoragePlace_Id(placeId);
     }
+
+    public ItemEntity addOneItem(Long itemId) {
+        ItemEntity item = getItem(itemId);
+        item.setQuantity(item.getQuantity() + 1);
+        return save(item);
+    }
+
+    public ItemEntity removeOneItem(Long itemId) {
+        ItemEntity item = getItem(itemId);
+        if (item.getQuantity() == 0) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    String.format("Item %d has no items to remove", itemId)
+            );
+        }
+        item.setQuantity(item.getQuantity() - 1);
+        return save(item);
+    }
 }
