0becbc53e69762a535e41449794f1f36c6b91d1c
krzk426 <krzk426@gmail.com>
2022-10-15 10:24:56 +0200

    item entity add field notifyQuantity, remove unused functions

--- src/main/java/cloud/ptl/indexer/model/ItemEntity.java
+++ src/main/java/cloud/ptl/indexer/model/ItemEntity.java
@@ -35,20 +35,10 @@ public class ItemEntity {
     private LocalDate dueDate;
     @ManyToOne
     private CategoryEntity category;
-
-    public String stringRepresentationWithDueDate() {
-        String itemDescription = this.getName();
-        if (this.getStoragePlace() != null) {
-            itemDescription += " stored in " + this.getStoragePlace();
-        }
-        itemDescription += " with due date: " + this.getDueDate();
-        return itemDescription;
-    }
-
+    private Integer notifyQuantity;
     public Integer incrementQuantity() {
         return ++this.quantity;
     }
-
     public Integer decrementQuantity() {
         return --this.quantity;
     }
